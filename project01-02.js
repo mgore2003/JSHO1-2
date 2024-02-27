93a3e201f441a767024226e05ac8998edb8e67e1
mgore2003 <142228045+mgore2003@users.noreply.github.com>
2024-02-27 09:08:27 -0500

    Add files via upload

---
+++ project01-02.js
@@ -0,0 +1,19 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Mark Gore
+    Date:  02/27/2024
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+let service1Name = "Basic";
+let service1Speed = "0 Mbps";
+let service2Name = "Express";
+let service2Speed = "100 Mbps";
+let service3Name = "Extreme";
+let service3Speed = "500 Mbps";
+let service4Name = "Ultimate";
+let service4Speed = "1 Gig";
